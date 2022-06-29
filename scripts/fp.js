29b924e50375ce4f76b6fd66a839070423473e3e
Hovag <hovoapelian@gmail.com>
2022-06-28 18:18:23 -0700

    updates

--- scripts/fp.js
+++ scripts/fp.js
@@ -2,12 +2,15 @@ const homeButton = document.getElementsByClassName('homeButton')[0]
 const projectsButton = document.getElementsByClassName('projectsButton')[0]
 const aboutButton = document.getElementsByClassName('aboutButton')[0]
 const navBar = document.getElementsByClassName('navBar')[0]
+const nameLogo = document.getElementsByClassName('name-logo')[0]
+const nameLogoDynamic = document.getElementsByClassName('name-logo-dynamic')[0]
+const blinkingCursor = document.getElementsByClassName('blinking-cursor')[0]
 
 new fullpage('#fullPage', {
     autoScrolling: true,
     menu: '#menu',
 	lockAnchors: false,
-    anchors:['s1', 's2', 's3'],
+    anchors:['home', 'projects', 'about'],
 	navigation: true,
 	navigationPosition: 'right',
 	navigationTooltips: ['firstSlide', 'secondSlide'],
@@ -22,18 +25,21 @@ new fullpage('#fullPage', {
             aboutButton.classList.remove("active")
             navBar.classList.remove("ligthBackground")
             navBar.classList.add("darkBackground")
+            nameLogoDynamic.textContent = "Home>"
         } else if (destination.index == 1) {
             homeButton.classList.remove("active")
             projectsButton.click()
             aboutButton.classList.remove("active")
             navBar.classList.remove("darkBackground")
             navBar.classList.add("lightBackground")
+            nameLogoDynamic.textContent = "Projects>"
         } else if (destination.index == 2) {
             homeButton.classList.remove("active")
             projectsButton.classList.remove("active")
             aboutButton.click()
             navBar.classList.remove("ligthBackground")
             navBar.classList.add("darkBackground")
+            nameLogoDynamic.textContent = "About>"
         }
     }
 })
