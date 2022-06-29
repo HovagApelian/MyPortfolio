018fd9b14dfaaf3f8b2ccc579f87ff72e479584a
Hovag Apelian <hovaggaro.apelian@calbaptist.edu>
2022-06-29 00:17:58 -0700

    mad updates

--- scripts/fp.js
+++ scripts/fp.js
@@ -4,6 +4,7 @@ const aboutButton = document.getElementsByClassName('aboutButton')[0]
 const navBar = document.getElementsByClassName('navBar')[0]
 const nameLogo = document.getElementsByClassName('name-logo')[0]
 const nameLogoDynamic = document.getElementsByClassName('name-logo-dynamic')[0]
+const nameLogoDynamic2 = document.getElementsByClassName('name-logo-dynamic')[1]
 const blinkingCursor = document.getElementsByClassName('blinking-cursor')[0]
 
 new fullpage('#fullPage', {
@@ -18,28 +19,106 @@ new fullpage('#fullPage', {
 	slidesNavigation: true,
 	slidesNavPosition: 'bottom',
     controlArrows: false,
-    onLeave: function(origin, destination, direction, trigger){
+    onLeave: function(origin, destination, direction, trigger) {
         if (destination.index == 0) {
             homeButton.click()
             projectsButton.classList.remove("active")
             aboutButton.classList.remove("active")
             navBar.classList.remove("ligthBackground")
             navBar.classList.add("darkBackground")
-            nameLogoDynamic.textContent = "Home>"
         } else if (destination.index == 1) {
             homeButton.classList.remove("active")
             projectsButton.click()
             aboutButton.classList.remove("active")
             navBar.classList.remove("darkBackground")
             navBar.classList.add("lightBackground")
-            nameLogoDynamic.textContent = "Projects>"
         } else if (destination.index == 2) {
             homeButton.classList.remove("active")
             projectsButton.classList.remove("active")
             aboutButton.click()
             navBar.classList.remove("ligthBackground")
             navBar.classList.add("darkBackground")
+        }
+    }, 
+    afterLoad: function(origin, destination, direction, trigger) {
+        nameLogoDynamic2.textContent = ""
+        if (destination.index == 0) {
+            nameLogoDynamic.textContent = "Home>"
+        } else if (destination.index == 1) {
+            activeSlide = fullpage_api.getActiveSlide().index
+            if (activeSlide == 0) {
+                nameLogoDynamic.textContent = "Projects\\Guavah>"
+            } else if (activeSlide == 1) {
+                nameLogoDynamic.textContent = "Projects\\Sensible>"
+            } else if (activeSlide == 2) {
+                nameLogoDynamic.textContent = "Projects\\Research Paper>"
+            }
+        } else if (destination.index == 2) {
             nameLogoDynamic.textContent = "About>"
+        }
+    },
+    afterSlideLoad: function(section, origin, destination, direction, trigger) {
+        nameLogoDynamic2.textContent = ""
+        if (section.anchor == 'projects') {
+            if (destination.index == 0) {
+                nameLogoDynamic.textContent = "Projects\\Guavah>"
+            } else if (destination.index == 1) {
+                nameLogoDynamic.textContent = "Projects\\Sensible>"
+            } else if (destination.index == 2) {
+                nameLogoDynamic.textContent = "Projects\\Research Paper>"
+            }
+        }
+    },
+    beforeLeave (origin, destination, direction, trigger) {
+        if (origin.index == 0) {
+            if (destination.index == 1) {
+                nameLogoDynamic2.textContent = "cd ..\\Projects\\"
+            } else if (destination.index == 2) {
+                nameLogoDynamic2.textContent = "cd ../About"
+            }
+        }
+
+        if (origin.index == 1) {
+            if (destination.index == 0) {
+                nameLogoDynamic2.textContent = "cd ..\\..\\Home"
+            } else if (destination.index == 2) {
+                nameLogoDynamic2.textContent = "cd ..\\..\\About"
+            }
+        }
+
+        if (origin.index == 2) {
+            if (destination.index == 0) {
+                nameLogoDynamic2.textContent = "cd ..\\Home"
+            } else if (destination.index == 1) {
+                nameLogoDynamic2.textContent = "cd ..\\Projects\\"
+            }
+        }
+    },
+    onSlideLeave: function(section, origin, destination, direction, trigger){
+        if (section.anchor == 'projects') {
+            if (origin.index == 0) {
+                if (destination.index == 1) {
+                    nameLogoDynamic2.textContent = "cd ..\\Sensible"
+                } else if (destination.index == 2) {
+                    nameLogoDynamic2.textContent = "cd ..\\Research Paper"
+                }
+            }
+
+            if (origin.index == 1) {
+                if (destination.index == 0) {
+                    nameLogoDynamic2.textContent = "cd ..\\Guavah"
+                } else if (destination.index == 2) {
+                    nameLogoDynamic2.textContent = "cd ..\\Research Paper"
+                }
+            }
+
+            if (origin.index == 2) {
+                if (destination.index == 0) {
+                    nameLogoDynamic2.textContent = "cd ..\\Guavah"
+                } else if (destination.index == 1) {
+                    nameLogoDynamic2.textContent = "cd ..\\Sensible"
+                }
+            }
         }
     }
 })
--- scripts/launch.js
+++ scripts/launch.js
@@ -17,19 +17,19 @@ async function animateName() {
 
 animateName()
 
-async function cycleNameLogoColors() {
+/* async function cycleNameLogoColors() {
     
-    const colors = ['#F58C8C', '#F5A48C', '#F5AD8C', '#F5CB8C', '#F5D98C', '#D3F58C',
+    /*const colors = ['#F58C8C', '#F5A48C', '#F5AD8C', '#F5CB8C', '#F5D98C', '#D3F58C',
      '#AFF58C', '#8CF5B6', '#8CDEF5', '#8CB2F5', '#8C8FF5', '#AB8CF5', '#CB8CF5', 
-     '#F58CF0', '#F58CCC', '#F58CA6', '#F58CAD']
+     '#F58CF0', '#F58CCC', '#F58CA6', '#F58CAD'] 
 
     while (true) {
-        let random = Math.floor(Math.random() * 15)
+        let random = Math.floor(Math.random() * 2)
         nameLogo.style.color = colors[random]
         blinkingCursor.style.color = colors[random]
         nameLogoDynamic.style.color = colors[random]
-        await sleep(3)
+        await sleep(2)
     }
 }
 
-cycleNameLogoColors()
+cycleNameLogoColors() */
