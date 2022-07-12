8fe40bf0d66a9a027611fa46eecff5945601a1a1
Hovag <hovoapelian@gmail.com>
2022-07-12 16:00:05 -0700

    version 0.1

--- scripts/nav.js
+++ scripts/nav.js
@@ -7,3 +7,10 @@ $(document).ready(function(){
     $(this).addClass('active').siblings().removeClass('active')
 })
 
+const hamburgerButton = document.getElementsByClassName('hamburger-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+hamburgerButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
