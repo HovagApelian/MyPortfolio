3b45c28d0244ba4d2768259f8ae3991f6660fdd8
Hovag <hovoapelian@gmail.com>
2022-07-12 21:44:41 -0700

    version 0.5

--- scripts/launch.js
+++ scripts/launch.js
@@ -36,7 +36,7 @@ cycleText()
 setInterval(cycleText, 2000)
 
 //remove some animations on smaller screen sizes
-if (window.innerWidth < 900) {
+if (window.innerWidth < 1200) {
     publicationHeaderFont1.classList.remove("hidden")
     flexChildPublication2.classList.remove("hidden")
 }
