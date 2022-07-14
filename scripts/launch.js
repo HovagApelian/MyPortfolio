97901405dad431bd66a8d0438da2d616f8536aa7
Hovag <hovoapelian@gmail.com>
2022-07-13 17:34:57 -0700

    version 1.2

--- scripts/launch.js
+++ scripts/launch.js
@@ -23,8 +23,17 @@ animateName()
 
 
 //Cycle words - abour page
-const textList = ["Hello", "Բարեւ", "Hola", "مرحبا"]
 const cycleWords = document.getElementsByClassName("cycle-words")[0]
+const currentTime = new Date().getHours()
+let textList
+
+if (currentTime >= 4 && currentTime < 12) {
+    textList = ["Good morning", "Բարի լոյս", "Buenos Dias", "صباح الخير"]
+} else if (currentTime >= 16 && currentTime < 20) {
+    textList = ["Good evening", "Բարեի իրիկուն", "Buenos noches", "مساء الخير"]
+} else {
+    textList = ["Hello", "Բարեւ", "Hola", "مرحبا", "What's good", "سلام عليكم"]
+}
 
 let i = 0;
 const cycleText = () => {
@@ -33,7 +42,7 @@ const cycleText = () => {
 }
 
 cycleText()
-setInterval(cycleText, 2000)
+setInterval(cycleText, 3000)
 
 //remove some animations on smaller screen sizes
 if (window.innerWidth < 1000) {
